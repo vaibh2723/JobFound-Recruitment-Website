ad999f870db9409042b840648f3150d01d9148f6
vaibh2723 <vaibhavjaiswal2707@gmail.com>
2023-08-04 17:23:49 +0530

    search bar adding

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -45,10 +45,56 @@ const Search = () => {
             </span>
           </div>
           <span className="search-bar-button">
-            <Button>Submit</Button>
+            <Button >Submit</Button>
           </span>
         </div>
       </form>
+
+      <div className="search-bar-secDiv">
+        <div className="search-bar-single-search">
+
+          <label htmlFor="relevance" className="search-bar-label">
+            Sort by :
+          </label >
+          <select id="relevance" className="search-bar-select">
+            <option value="" > Relevance</option>
+            <option value="" > Inclusive</option>
+            <option value="" > Starts With</option>
+            <option value="" > Contains</option>
+          </select>
+
+        </div>
+
+        <div className="search-bar-single-search">
+
+          <label htmlFor="type-of" className="search-bar-label">
+            Job Type :
+          </label >
+          <select id="type-of" className="search-bar-select">
+            <option value="" > Internship</option>
+            <option value="" > Full time Job</option>
+            <option value="" > Internship with Job offer</option>
+          </select>
+
+        </div>
+
+        <div className="search-bar-single-search">
+
+          <label htmlFor="level" className="search-bar-label">
+            Experience :
+          </label >
+          <select id="level" className="search-bar-select">
+            <option value="" > Fresher </option>
+            <option value="" > 0-1 year</option>
+            <option value="" > 1-3 year </option>
+            <option value="" > 4-6 year</option>
+            <option value="" > 6+ year </option>
+          </select>
+
+        </div>
+
+        <span className="search-bar-clearall">Clear All</span>
+      </div>
     </div>
   );
 };
